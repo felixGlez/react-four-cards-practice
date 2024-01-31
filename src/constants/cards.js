a802d6c6bc0de3eae8d1ecd9fef67f858fea22e0
felixGlez <felixgonzalezgarcia2010@gmail.com>
2024-01-31 20:32:48 +0100

    Create cards.js

---
+++ src/constants/cards.js
@@ -0,0 +1,22 @@
+export const CARDS_INFO = [
+	{
+		title: 'Supervisor',
+		text: 'Monitors activity to identify project roadblocks',
+		icon: './images/icon-supervisor.svg'
+	},
+	{
+		title: 'Team Builder',
+		text: 'Scans our talent network to create the optimal team for your project',
+		icon: './images/icon-team-builder.svg'
+	},
+	{
+		title: 'Karma',
+		text: 'Regularly evaluates our talent to ensure quality',
+		icon: './images/icon-karma.svg'
+	},
+	{
+		title: 'Calculator',
+		text: 'Uses data from past projects to provide better delivery estimates',
+		icon: './images/icon-calculator.svg'
+	}
+];
