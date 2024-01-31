49ed4596bde3d3a8493df5b04011d21036625808
felixGlez <felixgonzalezgarcia2010@gmail.com>
2024-01-31 21:28:09 +0100

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// import { v4 } from 'uuid';
 import Bar from './components/bar/Bar';
 import CardImg from './components/card-img/CardImg';
 import CardTitle from './components/card-title/CardTitle';
@@ -7,6 +8,7 @@ import MainContainer from './components/main-container/MainContainer';
 import MainWrapper from './components/main-wrapper/MainWrapper';
 import Text from './components/text/Text';
 import Title from './components/title/Title';
+// import { CARDS_INFO } from './constants/cards';
 
 const App = () => {
 	return (
@@ -57,6 +59,13 @@ const App = () => {
 						<CardImg src='./images/icon-calculator.svg' />
 					</Card>
 				</CardsContainer>
+
+				{/* {CARDS_INFO.map((card, index) => (
+					<CardsContainer key={v4()}>
+						<Bar class={card.barClass} />
+						<Card title={card.title} text={card.text} icon={card.icon} />
+					</CardsContainer>
+				))} */}
 			</MainContainer>
 		</MainWrapper>
 	);
--- src/constants/cards.js
+++ src/constants/cards.js
@@ -2,21 +2,25 @@ export const CARDS_INFO = [
 	{
 		title: 'Supervisor',
 		text: 'Monitors activity to identify project roadblocks',
-		icon: './images/icon-supervisor.svg'
+		icon: './images/icon-supervisor.svg',
+		barClass: 'green'
 	},
 	{
 		title: 'Team Builder',
 		text: 'Scans our talent network to create the optimal team for your project',
-		icon: './images/icon-team-builder.svg'
+		icon: './images/icon-team-builder.svg',
+		barClass: 'red'
 	},
 	{
 		title: 'Karma',
 		text: 'Regularly evaluates our talent to ensure quality',
-		icon: './images/icon-karma.svg'
+		icon: './images/icon-karma.svg',
+		barClass: 'yellow'
 	},
 	{
 		title: 'Calculator',
 		text: 'Uses data from past projects to provide better delivery estimates',
-		icon: './images/icon-calculator.svg'
+		icon: './images/icon-calculator.svg',
+		barClass: 'blue'
 	}
 ];
